7219b8483cfc9395020fd13fa4c413f4bc746e1e
Chris Francart <franchert@gmail.com>
2015-11-05 18:17:37 +0000

    default htaccess and settings files

---
+++ settings.default.php
@@ -0,0 +1,24 @@
+<?php
+//base site takes into account the path from the server root to the web root.
+$base_site="/var/www/cf-9/";
+//file base takes into account the path from the web root to the homepage.
+$file_base="";
+//base url is what you would type in your browser to visit the site
+$base_url="";
+$url_remove="";
+$starting = 1;
+//VPMDEV defaults
+	//$base_site="";
+	//$url_remove="/home/vpadmin/url.vpmdev.com/";
+	//$starting = 4;
+// apache default setup (is this working?)
+	//$base_site="/var/www/html/sitename";
+	//$file_base="/sitename";
+	//$url_remove="/html/";
+	//$starting = 2;
+	//ALSO, /sitename needs to be added to file calls through htaccess
+
+
+//debugging server variables
+//include_once($_SERVER['DOCUMENT_ROOT'].$file_base.'/test.php');
+?>
