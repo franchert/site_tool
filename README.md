42060bb07e1497ba67937c0c433e0dc56cbfe754
Chris Francart <franchert@gmail.com>
2015-12-01 16:31:32 +0000

    updates to homepage default text, formatting of markdown

--- README.md
+++ README.md
@@ -6,22 +6,22 @@ Dynamic front-end site prototype following the principles of [Atomic Design][htt
 
 Each index file pulls in a specified 'layout' file, which represents the front-end code. The layout file then pulls in the header and footer. The header pulls in stylesheets and script files.
 
-It also pulls in a 'director' file, which represents the dynamic and back-end components. The director file pulls in components functions, which are organized into groups within the components folder. It also pulls in global functions (which are functions that don't directly impact content but used many places), as well as a variables file, which calculates dynamic per-page variables such as path, page titles, section and mini-site information.
+It also pulls in a 'director' file, which represents the dynamic and back-end components. The director file pulls in components functions, which are organized into groups within the components directory. It also pulls in global functions (which are functions that don't directly impact content but used many places), as well as a variables file, which calculates dynamic per-page variables such as path, page titles, section and mini-site information.
 
-'''
+---
 
 ##TODO:
 
 - Add additional components to examples pages (colorbox, profile, subsection, spotlight)
-- Style .sitemap page
+- Explain process and how to get started using the tool.
 - Figure out a way to not need numbering on directories for ordering purposes within subnav/main nav
 - Print Stylesheets
 
-'''
+---
 
 ##Specifics:
 - Functions are called within each index. Each function builds upon a string representing a specific region within the specified layout. 
-- Main navigation is built from the numbered folders on the root. (skips 0 examples folder)
+- Main navigation is built from the numbered directories on the root. (skips directories beginning with a . or a 0)
 - Dynamic navigation (main/mini/subnav) skips all "dot" directories.
 - .sitemap directory creates a full nested sitemap.
 
--- index.php
+++ index.php
@@ -9,21 +9,19 @@ $instafeed = true;
 /*promo director initializes region variables and includes all the functions to create elements*/
 include_once($_SERVER['DOCUMENT_ROOT'].$file_base."/.includes/director.php");
 
-$p_one .="<script src='https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js'></script>";
 $p_one .="<div class='wrapper'><div class='column'>";
-$p_one .="<h2>Welcome to <span style='color:blue;text-shadow:2px 2px 0px rgba(255,0,0,1);width:auto;float:none;display:inline-block;'>Build-O-Tron</span></h2>";
+$p_one .="<h2>Welcome to <span style='color:blue;text-shadow:2px 2px 0px rgba(255,0,0,1),4px 4px 0px rgba(0,255,0,1);width:auto;float:none;display:inline-block;'>Build-O-Tron</span></h2>";
 $p_one .="<p>Build-O-Tron has been developed to use as a front-end framework specific to VisionPoint Marketing website designs. It uses specific components based on typical higher education use cases to put together a prototype front-end for provided designs.</p>";
 $p_one .="<div style='width:300px;padding-right:20px;'>".p_promo("button","image","light","Examples Page",null,null,null,"/0examples")."</div>";
 $p_one .="<p>The link to the left is an example of a default implementation of a button promo, and takes you to the examples section, showing off other examples of components included in this system.</p>";
-$p_one .="</div></div>";
-$p_one .="<hr>";
-$p_one .="<div class='wrapper'><div class='column'>";
-$p_one .="<p>This page also includes front-end guidelines used on all VisionPoint projects. These guidelines should be followed for all front-end projects, and should be followed for all Drupal and WordPress back-end projects as closely as the chosen CMS allows.</p>";
-$p_one .="<h2>Markup</h2>";
-$p_one .="<h3>Doctype</h3>";
-$p_one .="<p>the HTML5 doctype will be used unless a separate doctype declaration is required</p>";
-$p_one .="<pre class='prettyprint'>&lt;!DOCTYPE html&gt;</pre>";
+$p_one .="<p>Examples of the code used to create these components and how the system works can be found in the README.md files in each directory.</p>";
+$p_one .="<h2>Getting Started</h2>";
+$p_one .="<p>The file structure that this system uses replicates the sitemap created in the information architecture phase of the project. Creating a new directory will place it within the directory structure in alphabetical order. Creating an index.php file inside of that directory will display the contents of that file when accessing it via the web.</p>";
+$p_one .="<p>Each index.php file pulls in sitewide variables, sets page specific variables and builds a string to display within regions on a layout defined by these variables. Each file has three major sections:</p><ol style='margin-bottom:30px;list-style:decimal;margin-left:30px;'><li>Set Variables</li><li>Initialize and build strings</li><li>Print out strings within defined layout</li></ol>";
+$p_one .="<p>Initializing and printing the strings are done through includes files, but the variables and strings need to be built manually to show the content for each specific page</p>";
 $p_one .="</div></div>";
 
+$p_one .="<hr>";
+$p_one .="<p>Front-End guidelines can be found <a href='visionpointmarketing.github.io'>here</a></p>";
 include_once($_SERVER['DOCUMENT_ROOT'].$file_base."/.includes/layouts/".$layout."/".$layout.".php");
 ?>
